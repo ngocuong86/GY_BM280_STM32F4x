85e5449f7b19b0aec608d77e409e5f40c1e2c1b5
Ngo Van Cuong <cuongnv@lumi.vn>
2022-07-19 17:09:10 +0700

    Commit CleanCode

--- Src/Driver/SPI1-Interface/SPI1-Interface.h
+++ Src/Driver/SPI1-Interface/SPI1-Interface.h
@@ -32,7 +32,20 @@
 /******************************************************************************/
 /*                     EXPORTED TYPES and DEFINITIONS                         */
 /******************************************************************************/
-
+#define SPI1_CS_PORT			GPIOB
+#define SPI1_CS_PIN				GPIO_Pin_6
+#define SPI1_RST_PORT			GPIOC
+#define SPI1_RST_PIN			GPIO_Pin_7
+#define SPI1_MOSI_PORT			GPIOA
+#define SPI1_MOSI_PIN			GPIO_Pin_7
+#define SPI1_SCK_PORT			GPIOA
+#define SPI1_SCK_PIN			GPIO_Pin_5
+#define SPI1_RS_PORT			GPIOA
+#define SPI1_RS_PIN				GPIO_Pin_9
+#define SPI1_ENABLE_PORT		GPIOB
+#define SPI1_ENABLE_PIN			GPIO_Pin_10
+#define SPI1_MODE_PORT			GPIOA
+#define SPI1_MODE_PIN			GPIO_Pin_8
 /******************************************************************************/
 /*                              PRIVATE DATA                                  */
 /******************************************************************************/
